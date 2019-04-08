e32c5cf3a2a4736f16e2bc3e0adfc73b0789daa0
NancyBolstad <litchacha@gmail.com>
2019-04-08 12:42:49 +0200

    Fixed endpoint and the display of new rating

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,104 +1,125 @@
 import React, { Component } from "react";
+import {PLAYERS_ENDPOINT, CALCULATE_ELO_ENDPOINT} from "../util/constants"
 
 class Form extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        playerList: [],
-        requestHasError: false,
-        rating:""
-      };
-      this.setMyRating = this.setMyRating.bind(this);
-      this.setOpponentRating = this.setOpponentRating.bind(this);
-      this.setGameResult = this.setGameResult.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    componentDidMount() {
-      const url =
-        "https://us-central1-chessscore.cloudfunctions.net/getEloScoreBoard";
-  
-      (async () => {
-        try {
-          const data = await (await fetch(url)).json();
-          this.setState({
-            playerList: data
-          });
-        } catch (error) {
-          this.setState({
-            error: true
-          });
-        }
-      })();
-    }
-  
-    setMyRating(e) {
-      this.setState({ myRating: e.target.value });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerList: [],
+      requestHasError: false,
+      rating: ""
+    };
+    this.setMyRating = this.setMyRating.bind(this);
+    this.setOpponentRating = this.setOpponentRating.bind(this);
+    this.setGameResult = this.setGameResult.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.showNewRating=this.showNewRating.bind(this);
+  }
+  componentDidMount() {
+    const url = PLAYERS_ENDPOINT;
 
-    setOpponentRating(e) {
-        this.setState({ opponentRating: e.target.value });
+    (async () => {
+      try {
+        const data = await (await fetch(url)).json();
+        this.setState({
+          playerList: data
+        });
+      } catch (error) {
+        this.setState({
+          error: true
+        });
       }
+    })();
+  }
 
-      setGameResult(e) {
-        this.setState({ gameResult: e.target.value });
-      }
+  setMyRating(e) {
+    this.setState({ myRating: e.target.value });
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        const url=`https://us-central1-chessscore.cloudfunctions.net/getNewRating?myRating=${this.state.myRating}&opponentRating=${this.state.opponentRating}&myGameResult=${this.state.gameResult}`
-        fetch(url)
-        .then(response=>(response.json()))
-        .then(data=>this.setState({rating:data.newRating}))
+  setOpponentRating(e) {
+    this.setState({ opponentRating: e.target.value });
+  }
+
+  setGameResult(e) {
+    this.setState({ gameResult: e.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const url = `${CALCULATE_ELO_ENDPOINT}?myRating=${
+      this.state.myRating
+    }&opponentRating=${this.state.opponentRating}&myGameResult=${
+      this.state.gameResult
+    }`;
+
+    (async () => {
+      try {
+        const data = await (await fetch(url)).json();
+        this.setState({
+          rating: data.newRating
+        });
+      } catch (error) {
+        this.setState({
+          error: true
+        });
       }
-  
-    render() {
-      if (!this.state.error) {
-        const rating=this.state.rating;  
-        return (
-            <form onSubmit={this.handleSubmit}>
-        <label>
-          Spiller A:
-          <select onChange={this.setMyRating}>
-          <option value="" disabled selected>Choose your option</option>
-            {
-            this.state.playerList.map(item => (
-            <option value={item.rating}>
-              {item.name}
-            </option>
-          ))
-            }
-          </select>
-        </label>
-        <label>
+    })();
+  }
+
+  showNewRating(){
+      const rating = parseInt(this.state.rating);
+      if(rating>0 && !this.state.error){
+      console.log(typeof rating);
+      return `Din nye rating: ${rating}`;
+      }
+  }
+
+  render() {
+    if (!this.state.error) {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Spiller A:
+            <select onChange={this.setMyRating}>
+              <option value="" disabled selected>
+                Choose your option
+              </option>
+              {this.state.playerList.map(item => (
+                <option value={item.rating}>{item.name}</option>
+              ))}
+            </select>
+          </label>
+          <label>
             Spiller B:
             <select onChange={this.setOpponentRating}>
-          <option value="" disabled selected>Choose your option</option>
-            {
-            this.state.playerList.map(item => (
-            <option value={item.rating}>
-              {item.name}
-            </option>
-          ))
-            }
-          </select>
-        </label>
-        <label>
+              <option value="" disabled selected>
+                Choose your option
+              </option>
+              {this.state.playerList.map(item => (
+                <option value={item.rating}>{item.name}</option>
+              ))}
+            </select>
+          </label>
+          <label>
             Resultat:
             <select onChange={this.setGameResult}>
-          <option value="" disabled selected>Choose your option</option>
-          <option value="0">Tap spiller A</option>
-          <option value="0.5">Uavgjort</option>
-          <option value="1">Seier spiller A</option>
-          </select>
-        </label>
-        <input type="submit" value="Kalkuler ELO" />
-        <div>Din nye rating:{rating}</div>
-      </form>
-        );
-      } else {
-        return <div>Something wrong</div>;
-      }
+              <option value="" disabled selected>
+                Choose your option
+              </option>
+              <option value="0">Tap spiller A</option>
+              <option value="0.5">Uavgjort</option>
+              <option value="1">Seier spiller A</option>
+            </select>
+          </label>
+          <input type="submit" value="Kalkuler ELO" />
+          <div>{this.showNewRating()}</div>
+        </form>
+      );
+    } else {
+      return <div>Something wrong</div>;
     }
   }
-  
-  export default Form;
+}
+
+export default Form;
